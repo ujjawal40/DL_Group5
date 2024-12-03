704512f7bd63a9387119ddeeb8f0fbbd050df183
SaiAvinash-23 <saiavinashn@gwmail.gwu.edu>
2024-12-02 23:47:08 -0500

    Updated the main project file

--- project.py
+++ project.py
@@ -1,12 +1,1 @@
-# This is a test line
-
-# Added 2nd line
-
-# This is 3rd line
-
-# This is a 4th line change
-
-
-# This is a 5th line change
-
-# This is a 6th line change
+# This is the main file to run the project
