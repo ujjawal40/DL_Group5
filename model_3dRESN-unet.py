99de0eb59e5a4baf5c9ba5f474937984ef095186
bhagavathsai <48169735+bhagavathsai@users.noreply.github.com>
2024-12-08 20:00:36 -0500

    Update model_3dRESN-unet.py

--- model_3dRESN-unet.py
+++ model_3dRESN-unet.py
@@ -325,37 +325,7 @@ class OutConv(nn.Module):
     def forward(self, x):
         return self.conv(x)
 
-# class UNet3D(nn.Module):
-#     def __init__(self, n_channels, n_classes, bilinear=True):
-#         super(UNet3D, self).__init__()
-#         self.n_channels = n_channels
-#         self.n_classes = n_classes
-#         self.bilinear = bilinear
-#
-#         self.inc = DoubleConv(n_channels, 64)
-#         self.down1 = Down(64, 128)
-#         self.down2 = Down(128, 256)
-#         self.down3 = Down(256, 512)
-#         self.down4 = Down(512, 512)
-#         self.up1 = Up(1024, 256, bilinear)
-#         self.up2 = Up(512, 128, bilinear)
-#         self.up3 = Up(256, 64, bilinear)
-#         self.up4 = Up(128, 64, bilinear)
-#         self.outc = OutConv(64, n_classes)
-#
-#     def forward(self, x):
-#         x1 = self.inc(x)
-#         x2 = self.down1(x1)
-#         x3 = self.down2(x2)
-#         x4 = self.down3(x3)
-#         x5 = self.down4(x4)
-#         x = self.up1(x5, x4)
-#         x = self.up2(x, x3)
-#         x = self.up3(x, x2)
-#         x = self.up4(x, x1)
-#         logits = self.outc(x)
-#         return logits
-#
+
 
 #%%%%
 
@@ -507,13 +477,13 @@ val_loader = DataLoader(val_dataset, batch_size=1, shuffle=False, collate_fn=cus
 
 
 sav_dir='/home/ubuntu/dl_code/models'
-# model = UNet3D(n_channels=4, n_classes=5)  # Adjust the number of channels and classes based on your dataset specifics
+
 model = ResidualUNet3D(n_channels=4, n_classes=5)
 model = model.to(device)
 criterion = torch.nn.CrossEntropyLoss()  # You might adjust this depending on your specific task
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 
-# train_model(model, data_loader, val_loader, criterion, optimizer, num_epochs=20, save_directory=sav_dir,num_classes=5)
+
 history = train_model(model, data_loader, val_loader, criterion, optimizer, 20, sav_dir, 5, 5)
 
 
